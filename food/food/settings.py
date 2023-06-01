d0a3789a0fcf179e7de45571a9be74a44cfb2b62
Дима <paradicedae@gmail.com>
2023-06-01 17:00:51 +0300

    dishes

--- food/food/settings.py
+++ food/food/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foodfinder'
+    'foodfinder',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/index"
+
+
